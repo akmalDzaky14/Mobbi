0dfd6e2be5578aed5bc2f4ff86379069a841d58c
Akmal Dzaky <akmaldzaky33@gmail.com>
2023-09-16 12:39:18 +0700

    Init commit

---
+++ NodeJs/main.js
@@ -0,0 +1,6 @@
+/**
+ * TUGAS
+ * - buatlah sistem yang menerima user input berupa nilai mata kuliah
+ * - ubahlah nilai tersebut dari angka menjadi huruf dari A sampai F
+ * - user tidak boleh menginput angka dibawah 0 atau di atas 100
+ */
